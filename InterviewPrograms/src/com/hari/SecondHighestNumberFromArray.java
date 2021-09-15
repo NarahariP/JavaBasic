61bb039800f6cbb57f5bf65e592ef5397ec565d6
NarahariP <achitanarahari@gmail.com>
2021-09-15 22:57:29 +0530

    commit;

--- InterviewPrograms/src/com/hari/SecondHighestNumberFromArray.java
+++ InterviewPrograms/src/com/hari/SecondHighestNumberFromArray.java
@@ -1,5 +1,8 @@
 package com.hari;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class SecondHighestNumberFromArray {
 
 	private static int getSecondHighestNumber(int[] input) {
@@ -19,5 +22,13 @@ public class SecondHighestNumberFromArray {
 	public static void main(String[] args) {
 		int input[] = { 100, 44, 67, 43, 67, 29, 22, 97, 54, 56, 33, 99, 105, 110 };
 		System.out.println(getSecondHighestNumber(input));
+
+		// Java 8
+		System.out.println(Arrays.stream(input) // returns IntStream
+				.boxed()// converts IntStream into Steam<Integer>
+				.sorted(Collections.reverseOrder())// sorting with reverseOrder
+				.limit(2)// holding first two elements
+				.skip(1)// ignoring 1st one
+				.findFirst().get());// finally reading reading remaining element as Optional<Integer>
 	}
 }
