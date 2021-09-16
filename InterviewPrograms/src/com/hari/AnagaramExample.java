292be4934c77e62ea3610a3fe6a7914597c3176a
NarahariP <achitanarahari@gmail.com>
2021-09-16 21:45:18 +0530

    commit;

--- InterviewPrograms/src/com/hari/AnagaramExample.java
+++ InterviewPrograms/src/com/hari/AnagaramExample.java
@@ -1,6 +1,8 @@
 package com.hari;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class AnagaramExample {
 	private static boolean isAnagaram(String input1, String input2) {
@@ -21,10 +23,25 @@ public class AnagaramExample {
 		return isAnagaram;
 	}
 
+	private static boolean isAnagaram2(String input1, String input2) {
+		List<String> listWord1 = Arrays.asList(input1.toLowerCase().split(""));
+		List<String> listWord2 = Arrays.asList(input2.toLowerCase().split(""));
+		Collections.sort(listWord1);
+		Collections.sort(listWord2);
+		input1 = String.join("", listWord1);
+		input2 = String.join("", listWord2);
+		return input1.equals(input2);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(isAnagaram("silent", "liste"));
 		System.out.println(isAnagaram("silent", "listen"));
 		System.out.println(isAnagaram("Silent", "listen"));
 		System.out.println(isAnagaram("silentz", "listeny"));
+		System.out.println("==========================");
+		System.out.println(isAnagaram2("silent", "liste"));
+		System.out.println(isAnagaram2("silent", "listen"));
+		System.out.println(isAnagaram2("Silent", "listen"));
+		System.out.println(isAnagaram2("silentz", "listeny"));
 	}
 }
