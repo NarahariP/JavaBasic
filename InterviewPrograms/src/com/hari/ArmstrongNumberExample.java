cd5ef3faf273e495b8de156e726fb3407ab7fb39
NarahariP <achitanarahari@gmail.com>
2021-09-17 21:20:27 +0530

    commit.

--- InterviewPrograms/src/com/hari/ArmstrongNumberExample.java
+++ InterviewPrograms/src/com/hari/ArmstrongNumberExample.java
@@ -1,5 +1,7 @@
 package com.hari;
 
+import java.util.stream.IntStream;
+
 public class ArmstrongNumberExample {
 
 	private static boolean isArmstrong(int number) {
@@ -22,9 +24,26 @@ public class ArmstrongNumberExample {
 		return isArmstrong;
 	}
 
+	// Java 8
+	public static boolean isArmstrongInJava8(int number) {
+		return IntStream.builder().add(number).build().allMatch(n -> {
+			int c = 0, temp = n;
+			while (n > 0) {
+				c += Math.pow(n % 10, Integer.toString(temp).length());
+				n = n / 10;
+			}
+			if (c == temp)
+				return true;
+			else
+				return false;
+		});
+	}
+
 	public static void main(String[] args) {
 		System.out.println(isArmstrong(153)); // true
 		System.out.println(isArmstrong(123)); // false
 		System.out.println(isArmstrong(1634)); // true
+
+		System.out.println(isArmstrongInJava8(153)); // true
 	}
 }
