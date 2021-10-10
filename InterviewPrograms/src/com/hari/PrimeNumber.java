3e7cd1ae6893909c3d131459db4745f8e5c907bf
NarahariP <achitanarahari@gmail.com>
2021-10-10 13:35:23 +0530

    commit.

--- InterviewPrograms/src/com/hari/PrimeNumber.java
+++ InterviewPrograms/src/com/hari/PrimeNumber.java
@@ -1,5 +1,7 @@
 package com.hari;
 
+import java.util.stream.IntStream;
+
 public class PrimeNumber {
 
 	private static boolean isPrime(int number) {
@@ -19,6 +21,48 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		System.out.println(isPrime(5));
 		printPrimeUptoNthNumber(100);
+		System.out.println();
+		printPrimeNumbersUpto1(100);
+		System.out.println();
+		printPrimeNumbersUpto2(100);
+		System.out.println();
+		printPrimeNumbersUpto3(100);
+		System.out.println();
+		printPrimeNumbersUpto4(100);
+	}
+
+	private static boolean primeCheckWithAnyMatch(int val) {
+		return !IntStream.rangeClosed(2, val / 2).anyMatch(i -> val % i == 0);
+	}
+
+	private static boolean primeCheckWithNoneMatch(int val) {
+		return IntStream.rangeClosed(2, val / 2).noneMatch(k -> val % k == 0);
+	}
+
+	private static void printPrimeNumbersUpto1(int val) {
+		IntStream.rangeClosed(2, val).filter(i -> !IntStream.rangeClosed(2, i / 2).anyMatch(k -> i % k == 0))
+				.forEach(v -> System.out.print(v + " "));
+	}
+
+	private static void printPrimeNumbersUpto2(int val) {
+		IntStream.rangeClosed(2, val).filter(i -> primeCheckWithAnyMatch(i)).forEach(v -> System.out.print(v + " "));
+		/* with method ref */
+		System.out.println();
+		IntStream.rangeClosed(2, val).filter(PrimeNumber::primeCheckWithAnyMatch)
+				.forEach(v -> System.out.print(v + " "));
+	}
+
+	private static void printPrimeNumbersUpto3(int val) {
+		IntStream.rangeClosed(2, val).filter(i -> !IntStream.rangeClosed(2, i / 2).anyMatch(k -> i % k == 0))
+				.forEach(v -> System.out.print(v + " "));
+	}
+
+	private static void printPrimeNumbersUpto4(int val) {
+		IntStream.rangeClosed(2, val).filter(i -> primeCheckWithNoneMatch(i)).forEach(v -> System.out.print(v + " "));
+		/* with method ref */
+		System.out.println();
+		IntStream.rangeClosed(2, val).filter(PrimeNumber::primeCheckWithNoneMatch)
+				.forEach(v -> System.out.print(v + " "));
 	}
 
 	private static void printPrimeUptoNthNumber(int number) {
