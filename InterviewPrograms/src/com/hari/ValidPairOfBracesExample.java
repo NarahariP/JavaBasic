c98b2670a80bf4741c597c646a431326ad4b7067
NarahariP <achitanarahari@gmail.com>
2021-10-16 12:43:20 +0530

    commit.

--- InterviewPrograms/src/com/hari/ValidPairOfBracesExample.java
+++ InterviewPrograms/src/com/hari/ValidPairOfBracesExample.java
@@ -1,13 +1,27 @@
 package com.hari;
 
+import java.util.function.Function;
+
 public class ValidPairOfBracesExample {
 
+	static int count = 0;
+	static Function<Character, String> function = ch -> {
+		if (ch == '{') {
+			count++;
+		} else if (count > 0 && ch == '}') {
+			count--;
+			return "Y";
+		}
+		return "N";
+	};
+
 	public static void main(String[] args) {
-		String input1 = "main{method() {System.out.println(\"okay\")}print() {testing}}";
+		String input1 = "main{method() {System{out{.println(\"okay\")}print() {testing}}";
 		String input2 = "main{method() {System{}out{.println(\"okay\")}print() {testing}}";
 		System.out.println(noOfValidPairs(input1));
 		System.out.println(noOfValidPairs(input2));
-
+		System.out.println(noOfValidPairsInJava8(input1));
+		System.out.println(noOfValidPairsInJava8(input2));
 	}
 
 	private static int noOfValidPairs(String input) {
@@ -24,4 +38,12 @@ public class ValidPairOfBracesExample {
 		}
 		return count;
 	}
+
+	private static long noOfValidPairsInJava8(String input) {
+		long count = input.chars().mapToObj(c -> {
+			return (char) c;
+		}).map(ch -> function.apply(ch)).filter(val -> "Y".equals(val)).count();
+		return count;
+	}
+
 }
